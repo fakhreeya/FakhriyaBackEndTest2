91a6102bb0aad410757069b2707b6ec6b3ff7139
fakhreeya <162126369+fakhreeya@users.noreply.github.com>
2024-03-04 18:31:17 +0500

    xatoge main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello BackEnd 1")
-	fmt.Println("Hello BackEnd 2")
+	fmt.Println("Hello BackEnd amina 2")
 	fmt.Println("Hello BackEnd 3 ")
 fmt.Println("--------------------------------------- ")
 fmt.Println("************************ ")
