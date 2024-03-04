0fca6b13d037b4d4c6ac41ebcb853625fbcc62f2
fakhreeya <162126369+fakhreeya@users.noreply.github.com>
2024-03-04 18:17:44 +0500

    Update main.go

--- main.go
+++ main.go
@@ -7,5 +7,5 @@ func main() {
 	fmt.Println("Hello BackEnd 2")
 	fmt.Println("Hello BackEnd 3 ")
 fmt.Println("--------------------------------------- ")
-
+fmt.Println("************************ ")
 }
